9ff3ba373eea5d701bb0333555130517d066968a
Ashish kumar <ashishraj916263@outlook.com>
2023-05-16 14:51:39 +0530

    commit update

--- AjaxCRUD/Models/Category.cs
+++ AjaxCRUD/Models/Category.cs
@@ -7,7 +7,7 @@ namespace AjaxCRUD.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Name is Required")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
         [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100 only!")]
